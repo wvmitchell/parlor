771b2f0fca38b3e71b2326add768e0b802861e12
Will Mitchell <wvmitchell@gmail.com>
2014-12-02 12:03:42 -0700

    working implementation of hangman

--- spec/hangman/game_spec.rb
+++ spec/hangman/game_spec.rb
@@ -14,4 +14,62 @@ describe Parlor::Hangman::Game do
     expect(game.hidden_word).to_not be nil
   end
 
+  it 'can accept a guess, and return the number of occurances' do
+    game.hidden_word = 'potato'
+    expect(game.guess('a')).to be 1
+    expect(game.guess('o')).to be 2
+    expect(game.guess('y')).to be 0
+  end
+
+  it 'returns the current state of the word' do
+    game.hidden_word = 'potato'
+    expect(game.current_word).to eq '______'
+    game.guess('a')
+    expect(game.current_word).to eq '___a__'
+    game.guess('o')
+    expect(game.current_word).to eq '_o_a_o'
+  end
+
+  it 'knows how many guesses are remaining for the current word' do
+    game.hidden_word = 'potato'
+    game.guess('a')
+    expect(game.remaining_guesses).to be 10
+    game.guess('m')
+    expect(game.remaining_guesses).to be 9
+  end
+
+  it 'doesnt penalize the same wrong guess twice' do
+    game.hidden_word = 'potato'
+    game.guess('y')
+    game.guess('y')
+    expect(game.remaining_guesses).to be 9
+  end
+
+  it 'accepts full word guesses' do
+    game.hidden_word = 'potato'
+    game.guess('racers')
+    expect(game.won?).to be false
+    game.guess('potato')
+    expect(game.won?).to be true
+  end
+
+  it 'recognizes a win state' do
+    game.hidden_word = 'potato'
+    game.guess('p')
+    game.guess('o')
+    game.guess('t')
+    expect(game.won?).to be false
+    game.guess('a')
+    expect(game.won?).to be true
+  end
+
+  it 'recognizes a loss state' do
+    game.hidden_word = 'potato'
+    expect(game.lost?).to be false
+    'qweryuilkj'.chars.each do |letter|
+      game.guess(letter)
+    end
+    expect(game.lost?).to be true
+  end
+
 end
